e78401ecdf2463781fa319218d5a2f998ca4f437
marzanulhoque <marzanulru17@gmail.com>
2020-12-07 07:54:26 +0600

    Template created

--- mainsite/ecommerce/models.py
+++ mainsite/ecommerce/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.IntegerField()
+    img = models.ImageField(upload_to='pic')
+
+
+
