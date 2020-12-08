3fe7e38e0fafc5c6568b26a1988c4352f6fd49e8
marzanulhoque <marzanulru17@gmail.com>
2020-12-08 10:24:27 +0600

    PRoduct created and rendered from Database

--- mainsite/ecommerce/models.py
+++ mainsite/ecommerce/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.IntegerField()
     img = models.ImageField(upload_to='pic')
 
-
-
+    def __str__(self):
+        return self.name
