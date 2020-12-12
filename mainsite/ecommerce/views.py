ee572897f459e1519c2c886c95736f1b3e555872
marzanulhoque <marzanulru17@gmail.com>
2020-12-12 10:05:20 +0600

    login system added

--- mainsite/ecommerce/views.py
+++ mainsite/ecommerce/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -7,3 +8,7 @@ def home(request):
     products = Product.objects.all()
 
     return render(request, 'ecommerce/home.html', {'products': products})
+
+
+def register(request):
+    return render(request, 'registration/register.html')
--- mainsite/mainsite/urls.py
+++ mainsite/mainsite/urls.py
@@ -19,7 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('ecommerce.urls'))
+                  path('admin/', admin.site.urls),
+                  path('', include('ecommerce.urls')),
+                  path('accounts/', include('django.contrib.auth.urls'))
 
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
