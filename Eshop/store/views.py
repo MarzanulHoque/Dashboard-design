d8c5bbe69c56fb05baf0aa2ab05b3d41917c909c
marzanulhoque <marzanulru17@gmail.com>
2020-12-14 12:38:18 +0600

    Getting Products Database

--- Eshop/store/views.py
+++ Eshop/store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models.product import Product
 
 
 def index(request):
-    return render(request, 'index.html')
+    products = Product.get_all_products()
+
+
+    return render(request, 'index.html', {'products': products})
