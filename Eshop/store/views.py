8524cd13219778365d3b7834d456973b3a5d90d1
marzanulhoque <marzanulru17@gmail.com>
2020-12-14 22:14:45 +0600

    Listing Category in Homepage

--- Eshop/store/views.py
+++ Eshop/store/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models.product import Product
+from .models.category import Category
 
 
 def index(request):
     products = Product.get_all_products()
+    categories = Category.get_all_categories()
 
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
 
-    return render(request, 'index.html', {'products': products})
+    return render(request, 'index.html', data)
