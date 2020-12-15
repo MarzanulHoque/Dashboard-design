f3bac0a9ffa82827eff02fabc5e2a2ff8da18dc8
marzanulhoque <marzanulru17@gmail.com>
2020-12-15 10:27:13 +0600

    Submitting from

--- Eshop/store/views.py
+++ Eshop/store/views.py
@@ -22,4 +22,7 @@ def index(request):
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method== 'GET' :
+
+        return render(request, 'signup.html')
+    else:
