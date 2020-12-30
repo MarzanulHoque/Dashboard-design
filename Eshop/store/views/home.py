9cc8d2c8d6b479253d3428af07e8c00415028ded
marzanulhoque <marzanulru17@gmail.com>
2020-12-30 07:44:53 +0600

    Cart Created Part 2

--- Eshop/store/views/home.py
+++ Eshop/store/views/home.py
@@ -8,11 +8,26 @@ class Index(View):
 
     def post(self, request):
         product = request.POST.get('product')
-        print(product)
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+
+            if quantity:
+                cart[product] = quantity + 1
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+        request.session['cart'] = cart
+
+        print(request.session['cart'])
         return redirect("homepage")
 
     def get(self, request):
+
         products = None
+
         categories = Category.get_all_categories()
 
         categoryID = request.GET.get('category')
@@ -24,6 +39,5 @@ class Index(View):
         data = {}
         data['products'] = products
         data['categories'] = categories
-        print('you are : ', request.session.get('email'))
 
         return render(request, 'index.html', data)
