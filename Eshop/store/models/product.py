1078447fed9286b59e922c952bcada09d8f7e2e3
marzanulhoque <marzanulru17@gmail.com>
2021-01-06 09:31:00 +0600

    Fetching cart products

--- Eshop/store/models/product.py
+++ Eshop/store/models/product.py
@@ -10,8 +10,12 @@ class Product(models.Model):
     image = models.ImageField(upload_to='uploads/products/')
 
     @staticmethod
+    def get_products_by_id(ids):
+        return Product.objects.filter(id__in = ids)
+
+    @staticmethod
     def get_all_products():
-        return Product.objects.all()\
+        return Product.objects.all()
 
     @staticmethod
     def get_all_products_by_categoryid(category_id):
