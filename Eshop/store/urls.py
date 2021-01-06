b2d27a48932fc5b3e401fab084ebb92c9c7aae47
marzanulhoque <marzanulru17@gmail.com>
2021-01-06 09:03:27 +0600

    Logout Operation Created

--- Eshop/store/urls.py
+++ Eshop/store/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from .views.signup import Signup
-from .views.login import Login
+from .views.login import Login, logout
 from .views.home import Index
 
 urlpatterns = [
     path('', Index.as_view(), name="homepage"),
     path('signup', Signup.as_view(), name="signup"),
-    path('login', Login.as_view(), name="login")
+    path('login', Login.as_view(), name="login"),
+    path('logout', logout, name="logout")
 ]
