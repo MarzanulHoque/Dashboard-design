32563df69b0255fced1251b3ac37c5e5f114570d
marzanulhoque <marzanulru17@gmail.com>
2021-01-07 10:43:36 +0600

    Showing Product In Cart Page

--- Eshop/store/templatetags/cart.py
+++ Eshop/store/templatetags/cart.py
@@ -11,6 +11,7 @@ def is_in_cart(product, cart):
             return True
     return False
 
+
 @register.filter(name='cart_quantity')
 def cart_quantity(product, cart):
     keys = cart.keys()
@@ -18,3 +19,15 @@ def cart_quantity(product, cart):
         if int(id) == product.id:
             return cart.get(id)
     return 0
+
+
+@register.filter(name='price_total')
+def price_total(product, cart):
+    return product.price * cart_quantity(product, cart)\
+
+@register.filter(name='total_cart_price')
+def total_cart_price(products, cart):
+    sum=0
+    for p in products:
+        sum += price_total(p,cart)
+    return sum
