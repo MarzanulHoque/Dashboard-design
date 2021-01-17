9a356cb3acdc4e164897b8dbf76b63430dd356a9
marzanulhoque <marzanulru17@gmail.com>
2021-01-17 09:39:34 +0600

    Adding Order Status

--- Eshop/store/views/orders.py
+++ Eshop/store/views/orders.py
@@ -12,5 +12,5 @@ class OrderView(View):
     def get(self, request):
         customer = request.session.get('customer')
         orders = Order.get_order_by_customer(customer)
-        print(orders)
+
         return render(request, 'orders.html', {'orders': orders})
