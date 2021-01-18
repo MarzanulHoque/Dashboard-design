3bb087735425014b712e93f91b3e52c6793ab15b
marzanulhoque <marzanulru17@gmail.com>
2021-01-18 11:00:51 +0600

    Alternative way to activate middleware

--- Eshop/store/urls.py
+++ Eshop/store/urls.py
@@ -5,6 +5,7 @@ from .views.home import Index
 from .views.cart import Cart
 from .views.checkout import CheckOut
 from .views.orders import OrderView
+from store.middlewares.auth import auth_middleware
 
 urlpatterns = [
     path('', Index.as_view(), name="homepage"),
@@ -13,5 +14,5 @@ urlpatterns = [
     path('logout', logout, name="logout"),
     path('cart', Cart.as_view(), name="cart"),
     path('check-out', CheckOut.as_view(), name="checkout"),
-    path('orders', OrderView.as_view(), name="orders")
+    path('orders',  auth_middleware(OrderView.as_view()), name="orders")
 ]
--- Eshop/store/views/orders.py
+++ Eshop/store/views/orders.py
@@ -6,11 +6,10 @@ from store.models.customer import Customer
 from store.models.product import Product
 from store.middlewares.auth import auth_middleware
 from store.models.orders import Order
-from django.utils.decorators import method_decorator
+
 
 class OrderView(View):
 
-    @method_decorator(auth_middleware)
     def get(self, request):
         customer = request.session.get('customer')
         orders = Order.get_order_by_customer(customer)
