cde11bfac9666757525e25764f65a38e1e14ae4b
marzanulhoque <marzanulru17@gmail.com>
2021-01-18 11:19:26 +0600

    Return Url

--- Eshop/store/middlewares/auth.py
+++ Eshop/store/middlewares/auth.py
@@ -3,8 +3,9 @@ from django.shortcuts import redirect
 
 def auth_middleware(get_response):
     def middleware(request):
+        returnurl = request.META['PATH_INFO']
         if not request.session.get('customer'):
-            return redirect('/login')
+            return redirect(f'/login?return_url={returnurl}')
 
         response = get_response(request)
         return response
